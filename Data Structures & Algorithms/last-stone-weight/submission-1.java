67b583d3d4793f02bd1da490977065b5e1125346
SABARI G <rohitsabari045@gmail.com>
2026-07-07 09:05:05 +0530

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+       PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);//heap structure
+
+       for(int stone : stones){
+            pq.offer(stone);//[1]
+       }
+
+       while(pq.size()>1){
+            int first = pq.poll();
+            int second = pq.poll();
+            if(first!=second){
+                pq.offer(first-second);
+            }
+        }
+       return pq.isEmpty()?0:pq.peek();
+    }
+}
