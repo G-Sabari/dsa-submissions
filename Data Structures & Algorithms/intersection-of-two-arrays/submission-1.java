ee6a2038e5bf8910510150dc7aea1df15f0ef812
SABARI G <rohitsabari045@gmail.com>
2026-07-08 23:39:33 +0530

    Add: intersection-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/intersection-of-two-arrays/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for(int num : nums1){
+            set1.add(num);//{1,2}
+        }
+
+        for(int num : nums2){
+            set2.add(num);//{2}
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int num : set1){
+            if(set2.contains(num)){
+                list.add(num);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+
+
+
+
+
+    }
+}
