2e07d8a1e42420823e2b3eec7faf061af58c0930
SABARI G <rohitsabari045@gmail.com>
2026-07-23 22:15:50 +0530

    Add: grumpy-bookstore-owner - submission-2

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-2.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int count = 0;
+        for(int i=0;i<customers.length;i++){
+            if(grumpy[i]==0){
+                count+=customers[i];
+            }
+        }
+
+        //first window
+        int extra = 0;
+        for(int i=0;i<minutes;i++){
+            if(grumpy[i]==1){
+                extra+=customers[i];
+            }
+        }
+        int best = extra;
+        //sliding window
+        for(int i=minutes;i<customers.length;i++){
+            if(grumpy[i-minutes]==1){
+                extra-=customers[i-minutes];
+            }
+
+            if(grumpy[i]==1){
+                extra+=customers[i];
+            }
+            best = Math.max(best,extra);
+        }
+        return best+count;
+    }
+}
