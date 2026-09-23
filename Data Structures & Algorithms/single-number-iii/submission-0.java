38bd88d12159bbfe8b3ddb2a05294f7cab5838a3
SABARI G <rohitsabari045@gmail.com>
2026-09-23 13:23:42 -0400

    Add: single-number-iii - submission-0

---
+++ Data Structures & Algorithms/single-number-iii/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            if(set.contains(num)){
+                set.remove(num);
+            } else{
+                set.add(num);
+            }
+        }
+
+        int[] ans = new int[2];
+
+        int i = 0;
+        for(int n : set){
+            ans[i] = n;
+            i++;
+        }
+
+        return ans;
+    }
+}
